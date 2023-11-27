5730d01a5c1ed8a905fa55a40864ad41fc3577d1
06776 <hajdarapatrik11@outlook.com>
2023-11-27 09:04:49 +0100

    Project init

---
+++ src/app/environments.ts
@@ -0,0 +1,13 @@
+export const Environments = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyB4p7yG5YacMxziMCa93iGFof9fBEq-dJg",
+        authDomain: "gallerydatabase-bb455.firebaseapp.com",
+        databaseURL: "https://gallerydatabase-bb455-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "gallerydatabase-bb455",
+        storageBucket: "gallerydatabase-bb455.appspot.com",
+        messagingSenderId: "402132652834",
+        appId: "1:402132652834:web:f31c05942b42eeb5e24b7b",
+        measurementId: "G-6NBTSL3XF0"
+    }
+}
